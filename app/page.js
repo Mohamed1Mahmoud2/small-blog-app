0af975d00d3e9d0f1448ee6810e85ce51cbcd38e
Mohamed Mahmoud <103222555+Mohamed1Mahmoud2@users.noreply.github.com>
2024-12-23 17:01:53 +0200

    update app-page m

--- app/page.js
+++ app/page.js
@@ -1,14 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 
 export default function Home() {
-  async function gitData() {
-    const dataRes = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const dataShow = await dataRes.json()
-    console.log(dataShow);
-  }
-  gitData()
+
   const mianDocTrStyle = {
     height: "200px",
     width: "70vw",
@@ -19,20 +15,29 @@ export default function Home() {
   return (
     <div>
       <h1>hi</h1>
-      <div style={mianDocTrStyle}>
-        <h2>programming</h2>
-        <p>what is the new in front end dev</p>
-        <p>what is the new in javascript</p>
-      </div>
-      <div style={mianDocTrStyle}>
-        <h2>food</h2>
-        <p>what is likes to eat</p>
-        <p>i love food</p>
-      </div>
-      <div style={mianDocTrStyle}>
-        <h2>sport</h2>
-        <p>play sport daily</p>
-      </div>
+      <Link href={'/posts/front-end-posts'}>
+        <div style={mianDocTrStyle}>
+          <h2>programming</h2>
+          <p>what is the new in front end dev</p>
+          <p>what is the new in javascript</p>
+        </div>
+      </Link>
+
+      <Link href={'/posts/food-posts'}>
+        <div style={mianDocTrStyle}>
+          <h2>food</h2>
+          <p>what is likes to eat</p>
+          <p>i love food</p>
+        </div>
+      </Link>
+
+      <Link href={'/posts/sport-posts'}>
+        <div style={mianDocTrStyle}>
+          <h2>sport</h2>
+          <p>play sport daily</p>
+        </div>
+      </Link>
+
     </div>
   );
 }
